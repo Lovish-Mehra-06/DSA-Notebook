c1d80cc327538b84a0d7422a4a7b4c277b8d0a75
LovishMehra <lovish06mehra@gmail.com>
2026-08-29 02:03:59 +0530

    math ques

---
+++ Math_Ques/Q1688_count-of-matches-in-tournament.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem Link : https://leetcode.com/problems/count-of-matches-in-tournament/
+ ! Problem Name : count-of-matches-in-tournament/
+ * Problem No.  : 1688
+ * Platform     : LeetCode
+ * Source       : Striver SDE Sheet
+ * Difficulty   : Easy
+ */
+
+class Solution
+{
+public:
+    int numberOfMatches(int n)
+    {
+        return n - 1;
+    }
+};
