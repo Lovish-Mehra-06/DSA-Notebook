9a6874a2830c13db4ec31bff20e6e1ca7ec66dde
LovishMehra <lovish06mehra@gmail.com>
2026-09-21 01:06:34 +0530

    add242. Valid Anagram

---
+++ 1_Arrays/Q242_Valid_Anagram.cpp
@@ -0,0 +1,55 @@
+/*
+ * Problem Info : 242. Valid Anagram
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Hash Set
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.size() != t.size())
+            return false;
+
+        int count[26] = {0};
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            count[tolower(s[i]) - 'a']++;
+            count[tolower(t[i]) - 'a']--;
+        }
+
+        // Check if all counts are zero
+        for (int i = 0; i < 26; i++)
+            if (count[i] != 0)
+                return false;
+
+        return true;
+    }
+};
+
+// Sorting:
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        int n = s.size();
+        int m = t.size();
+
+        if (n != m)
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if (s == t)
+            return true;
+
+        return false;
+    }
+};
