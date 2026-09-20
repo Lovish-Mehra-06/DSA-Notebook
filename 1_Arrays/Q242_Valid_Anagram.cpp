ef53f7e0e4096e3022e4e1d4d2b41152711595e9
LovishMehra <lovish06mehra@gmail.com>
2026-09-21 01:26:12 +0530

    add: implement transpose matrix solution and follow up of valid anagram

--- 1_Arrays/Q242_Valid_Anagram.cpp
+++ 1_Arrays/Q242_Valid_Anagram.cpp
@@ -53,3 +53,29 @@ public:
         return false;
     }
 };
+
+//!!! FOLLOW UP : What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        unordered_map<char, int> count;
+
+        if (s.size() != t.size())
+            return false;
+
+        for (char c : s)
+            count[c]++;
+
+        for (char c : t)
+        {
+            count[c]--;
+
+            if (count[c] < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
